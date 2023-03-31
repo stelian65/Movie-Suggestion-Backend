41500d87309e9687a16fd7868f7995ca17bc68d3
Steli <v.stely71@gmail.com>
2023-03-31 09:19:01 +0300

    Initial commit

---
+++ src/main/java/com/example/moviesuggestionbackend/MovieSuggestionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviesuggestionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieSuggestionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieSuggestionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moviesuggestionbackend/MovieSuggestionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviesuggestionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieSuggestionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
